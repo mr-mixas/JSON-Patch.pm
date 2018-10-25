a3eb790bd9bc2d787cb3c72e5b11fe5387fe8cb5
Michael Samoglyadov <mixas.sr@gmail.com>
2018-10-26 00:06:44 +0300

    0.04

--- lib/JSON/Patch.pm
+++ lib/JSON/Patch.pm
@@ -29,11 +29,11 @@ JSON::Patch - JSON Patch (rfc6902) for perl structures
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
